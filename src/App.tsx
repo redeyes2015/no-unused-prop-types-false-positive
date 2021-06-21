c75a58f23444f8a47ad0fda9815f27d1c8a5d8c2
redeyes2015 <redeyes0215@gmail.com>
2021-06-21 12:43:30 +0800

    Trigger false positive of react/no-unused-prop-types

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React, { FunctionComponent } from "react";
 import "./styles.css";
 import Slide from "./Slide";
 
@@ -5,12 +6,16 @@ interface SlideProps {
   isActive: boolean;
 }
 
+const MyCard: FunctionComponent<{ doo: boolean }> = ({ doo }) => {
+  return doo ? <div>1</div> : <div>0</div>;
+}
+
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
-      <Slide>{({ isActive }: SlideProps) => String(isActive)}</Slide>
+      <Slide>{({ isActive }: SlideProps) => <MyCard doo={isActive} /> }</Slide>
     </div>
   );
 }
